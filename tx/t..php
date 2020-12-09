23b0f3434c593d86226813a6d419c88f27475531
18840663488 <75653208+18840663488@users.noreply.github.com>
2020-12-09 20:35:21 +0800

    Add files via upload

---
+++ tx/t..php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<meta charset="UTF-8">
+<title>H</title>
+
+<link rel="stylesheet" href="/t/css/style.css">
+
+</head>
+<body>
+
+<script src='/t/js/p5.min.js'></script>
+<script src="/t/js/script.js"></script>
+
+</body>
+</html>
